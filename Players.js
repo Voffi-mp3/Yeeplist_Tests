7556d80c2898aa98440dd94421e419e1ae6b6fad
Voffi <Voffi910@outlook.com>
2026-08-16 19:10:57 +0100

    Refactor player and team data structure
    Updated player data with new attributes and scores.

--- Players.js
+++ Players.js
@@ -1,56 +1,74 @@
 export const players = [
   { 
     id:1, 
-    name:"Player", 
+    name:"Rank Badge Preview", 
     avatar:"../images/BlueYeep.png", 
-    title:"Gold III", 
-    score:45+0+3+0+0+6+0+4, 
-    rank:"../images/Gold.png",
+    title:"Text", 
+    score:1000, 
+    rank:"../images/GrandMaster.png",
     categories:[
-      {cat:"Bat PvP", pos:"Low Tier 1", img:"../images/Tiers/L1Bat.png"},
-      {cat:"Dagger", pos:"", img:"../images/Tiers.png"},
-      {cat:"Freeze Glove PvP", pos:"Low Tier: 4", img:"../images/Tiers/L4Snow.png"},
-      {cat:"Mace", pos:"", img:"../images/Tiers.png"},
-      {cat:"Shield", pos:"", img:"../images/Tiers.png"},
-      {cat:"Fire Axe PvP", pos:"Low Tier: 3", img:"../images/Tiers/L3Fire.png"},
-      {cat:"Scythe", pos:"", img:"../images/Tiers.png"},
-      {cat:"Other PvP", pos:"High Tier: 4", img:"../images/Tiers/H4Other.png"}
+      {cat:"Bat", pos:"", img:"../images/Bronze.png"},
+      {cat:"Dagger", pos:"", img:"../images/Silver.png"},
+      {cat:"Freeze Glove", pos:"", img:"../images/Gold.png"},
+      {cat:"Mace", pos:"", img:"../images/Platinum.png"},
+      {cat:"Shield", pos:"", img:"../images/Diamond.png"},
+      {cat:"Fire Axe", pos:"", img:"../images/Tiers/Ruby.png"},
+      {cat:"Scythe", pos:"", img:"../images/Master.png"},
+      {cat:"Other", pos:"", img:"../images/GrandMaster.png"}
     ]
   },
     { 
     id:2, 
-    name:"Player", 
+    name:"High Tier 1 Preview", 
     avatar:"../images/PeachYeep.png", 
     title:"Ruby II", 
     score:60+0+60+0+0+60+0+60, 
     rank:"../images/Ruby.png",
     categories:[
       {cat:"Bat PvP", pos:"High Tier 1", img:"../images/Tiers/H1Bat.png"},
-      {cat:"Dagger", pos:"", img:"../images/Tiers.png"},
-      {cat:"Freeze Glove PvP", pos:"High Tier: 1", img:"../images/Tiers/H1Snow.png"},
-      {cat:"Mace", pos:"", img:"../images/Tiers.png"},
-      {cat:"Shield", pos:"", img:"../images/Tiers.png"},
-      {cat:"Fire Axe PvP", pos:"High Tier: 1", img:"../images/Tiers/H1Fire.png"},
-      {cat:"Scythe", pos:"", img:"../images/Tiers.png"},
-      {cat:"Other PvP", pos:"High Tier: 1", img:"../images/Tiers/H1Other.png"}
+      {cat:"Dagger", pos:"High Tier 1", img:"../images/Tiers/H1Dagger.png"},
+      {cat:"Freeze Glove PvP", pos:"High Tier 1", img:"../images/Tiers/H1Snow.png"},
+      {cat:"Mace", pos:"High Tier 1", img:"../images/Tiers/H1Mace.png"},
+      {cat:"Shield", pos:"High Tier 1", img:"../images/Tiers/H1Shield.png"},
+      {cat:"Fire Axe PvP", pos:"High Tier 1", img:"../images/Tiers/H1Fire.png"},
+      {cat:"Scythe", pos:"High Tier 1", img:"../images/Tiers/H1Sythe.png"},
+      {cat:"Other PvP", pos:"High Tier 1", img:"../images/Tiers/H1Other.png"}
     ]
   },
       { 
-    id:2, 
+    id:3, 
     name:"Player", 
     avatar:"../images/PurpleYeep.png", 
-    title:"gold III", 
+    title:"Gold III", 
     score:10+0+2+0+0+4+0+45, 
     rank:"../images/Gold.png",
     categories:[
       {cat:"Bat PvP", pos:"High Tier 3", img:"../images/Tiers/H3Bat.png"},
       {cat:"Dagger", pos:"", img:"../images/Tiers.png"},
-      {cat:"Freeze Glove PvP", pos:"High Tier: 5", img:"../images/Tiers/H5Snow.png"},
+      {cat:"Freeze Glove PvP", pos:"High Tier 5", img:"../images/Tiers/H5Snow.png"},
       {cat:"Mace", pos:"", img:"../images/Tiers.png"},
       {cat:"Shield", pos:"", img:"../images/Tiers.png"},
-      {cat:"Fire Axe PvP", pos:"High Tier: 4", img:"../images/Tiers/H4Fire.png"},
+      {cat:"Fire Axe PvP", pos:"High Tier 4", img:"../images/Tiers/H4Fire.png"},
       {cat:"Scythe", pos:"", img:"../images/Tiers.png"},
-      {cat:"Other PvP", pos:"Low Tier: 1", img:"../images/Tiers/L1Other.png"}
+      {cat:"Other PvP", pos:"Low Tier 1", img:"../images/Tiers/L1Other.png"}
+    ]
+  },
+        { 
+    id:4, 
+    name:"Annonymos", 
+    avatar:"../images/Yeep.png", 
+    title:"Diamond I", 
+    score:10+30+45+0+20+30+0+45, 
+    rank:"../images/Gold.png",
+    categories:[
+      {cat:"Bat PvP", pos:"High Tier 3", img:"../images/Tiers/H3Bat.png"},
+      {cat:"Dagger PvP", pos:"High Tier 2", img:"../images/Tiers/H2Dagger.png"},
+      {cat:"Freeze Glove PvP", pos:"Low Tier 1", img:"../images/Tiers/L1Snow.png"},
+      {cat:"Mace", pos:"", img:"../images/Tiers.png"},
+      {cat:"Shield PvP", pos:"Low Tier 2", img:"../images/Tiers/L2Shield.png"},
+      {cat:"Fire Axe PvP", pos:"High Tier 2", img:"../images/Tiers/H2Fire.png"},
+      {cat:"Scythe", pos:"", img:"../images/Tiers.png"},
+      {cat:"Other PvP", pos:"Low Tier 1", img:"../images/Tiers/L1Other.png"}
     ]
   },
 
