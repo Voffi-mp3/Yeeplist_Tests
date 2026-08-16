99de6a938c2717d9de90ac0008fd11b5054d7666
Voffi <Voffi910@outlook.com>
2026-08-16 17:49:25 +0100

    Update Players.js

--- Players.js
+++ Players.js
@@ -3,11 +3,11 @@ export const players = [
     id:1, 
     name:"Player", 
     avatar:"../images/BlueYeep.png", 
-    title:"Gold II", 
-    score:6+20+0+0+30+0+0+20, 
+    title:"Gold III", 
+    score:45+0+3+0+0+6+0+4, 
     rank:"../images/Gold.png",
     categories:[
-      {cat:"Bat", pos:"", img:"../images/Tiers.png"},
+      {cat:"Bat PvP", pos:"Low Tier 1", img:"../images/Tiers/L1Bat.png"},
       {cat:"Dagger", pos:"", img:"../images/Tiers.png"},
       {cat:"Freeze Glove PvP", pos:"Low Tier: 4", img:"../images/Tiers/L4Snow.png"},
       {cat:"Mace", pos:"", img:"../images/Tiers.png"},
@@ -15,6 +15,42 @@ export const players = [
       {cat:"Fire Axe PvP", pos:"Low Tier: 3", img:"../images/Tiers/L3Fire.png"},
       {cat:"Scythe", pos:"", img:"../images/Tiers.png"},
       {cat:"Other PvP", pos:"High Tier: 4", img:"../images/Tiers/H4Other.png"}
+    ]
+  },
+    { 
+    id:2, 
+    name:"Player", 
+    avatar:"../images/PeachYeep.png", 
+    title:"Ruby II", 
+    score:60+0+60+0+0+60+0+60, 
+    rank:"../images/Ruby.png",
+    categories:[
+      {cat:"Bat PvP", pos:"High Tier 1", img:"../images/Tiers/H1Bat.png"},
+      {cat:"Dagger", pos:"", img:"../images/Tiers.png"},
+      {cat:"Freeze Glove PvP", pos:"High Tier: 1", img:"../images/Tiers/H1Snow.png"},
+      {cat:"Mace", pos:"", img:"../images/Tiers.png"},
+      {cat:"Shield", pos:"", img:"../images/Tiers.png"},
+      {cat:"Fire Axe PvP", pos:"High Tier: 1", img:"../images/Tiers/H1Fire.png"},
+      {cat:"Scythe", pos:"", img:"../images/Tiers.png"},
+      {cat:"Other PvP", pos:"High Tier: 1", img:"../images/Tiers/H1Other.png"}
+    ]
+  },
+      { 
+    id:2, 
+    name:"Player", 
+    avatar:"../images/PurpleYeep.png", 
+    title:"gold III", 
+    score:10+0+2+0+0+4+0+45, 
+    rank:"../images/Gold.png",
+    categories:[
+      {cat:"Bat PvP", pos:"High Tier 3", img:"../images/Tiers/H3Bat.png"},
+      {cat:"Dagger", pos:"", img:"../images/Tiers.png"},
+      {cat:"Freeze Glove PvP", pos:"High Tier: 5", img:"../images/Tiers/H5Snow.png"},
+      {cat:"Mace", pos:"", img:"../images/Tiers.png"},
+      {cat:"Shield", pos:"", img:"../images/Tiers.png"},
+      {cat:"Fire Axe PvP", pos:"High Tier: 4", img:"../images/Tiers/H4Fire.png"},
+      {cat:"Scythe", pos:"", img:"../images/Tiers.png"},
+      {cat:"Other PvP", pos:"Low Tier: 1", img:"../images/Tiers/L1Other.png"}
     ]
   },
 
