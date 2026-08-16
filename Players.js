efc7cc219c1028f2da7a3b3e8e4e1f9e2fae4596
Voffi <Voffi910@outlook.com>
2026-08-16 17:15:12 +0100

    Update Players.js

--- Players.js
+++ Players.js
@@ -7,14 +7,14 @@ export const players = [
     score:6+20+0+0+30+0+0+20, 
     rank:"../images/Gold.png",
     categories:[
-      {cat:"Bat PvP", pos:"Tier: 6", img:"https://yeeplist.page.gd/images/Tier6.png"},
-      {cat:"Dagger PvP", pos:"Tier: 4", img:"https://yeeplist.page.gd/images/Bat_Tier4.png"},
+      {cat:"Bat", pos:"", img:"../images/Tiers.png"},
+      {cat:"Dagger", pos:"", img:"../images/Tiers.png"},
       {cat:"Freeze Glove PvP", pos:"Low Tier: 4", img:"../images/Tiers/L4Snow.png"},
-      {cat:".M.", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:"Shield PvP", pos:"Tier: 3", img:"https://yeeplist.page.gd/images/Tier3.png"},
-      {cat:".F.", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:".S.", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:"Other PvP", pos:"Tier: 4", img:"https://yeeplist.page.gd/images/Tier4.png"}
+      {cat:"Mace", pos:"", img:"../images/Tiers.png"},
+      {cat:"Shield", pos:"", img:"../images/Tiers.png"},
+      {cat:"Fire Axe PvP", pos:"Low Tier: 3", img:"../images/Tiers/L3Fire.png"},
+      {cat:"Scythe", pos:"", img:"../images/Tiers.png"},
+      {cat:"Other PvP", pos:"High Tier: 4", img:"/images/Tiers/H4Other.png"}
     ]
   },
 
@@ -31,9 +31,9 @@ export const teams = [
     categories:[
       {cat:"#1 Player", pos:"285 Points", img:"../images/PeachYeep.png"},
       {cat:"#2 Player, Owner", pos:"180 Points", img:"../images/PurpleYeep.png"},
-      {cat:"#3 Unchosen", pos:"0 Points", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:"#4 Unchosen", pos:"0 Points", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:"#5 Unchosen", pos:"0 Points", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:"#3 Unchosen", pos:"0 Points", img:"../images/Yeep.png"},
+      {cat:"#4 Unchosen", pos:"0 Points", img:"../images/Yeep.png"},
+      {cat:"#5 Unchosen", pos:"0 Points", img:"../images/Yeep.png"},
     ]
   },
 
